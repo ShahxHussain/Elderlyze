6e3571d40a8f4ff080b54bf1a2325374430fa93a
ATIFSHAH159 <aatifshah15@gmail.com>
2025-08-25 10:48:24 -0700

    navbar updated

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Assets/Css/SignUp.css';
 import { Link } from 'react-router-dom';
-import Logo from '../Assets/Images/Logo.png';
+import Logo from '../Assets/Images/logo.png'
 
 function SignUp() {
   return (
